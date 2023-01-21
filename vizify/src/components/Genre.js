ce4930f1e2c5d26d685ac827f7f7cdf64b183496
RIGNITE <rck2001@hotmail.com>
2023-01-21 17:44:03 -0500

    updated genre js

--- vizify/src/components/Genre.js
+++ vizify/src/components/Genre.js
@@ -1,15 +1,29 @@
+import React from 'react'
 
-const Genres = () => {
-    state = {data: []}
+const Genre = () => {
+  state = {data: []}
 
-    componentDidMount() = () =>{
-        fetch('')
-          .then(response => response.text())
-          .then(data => Papa.parse(data, {
-            complete: this.updateData
-          }))
-      }
-    
-    
-    
+  componentDidMount() = () => {
+    fetch('./top50_Spotify_final_data')
+      .then(response => response.text())
+      .then(data => Papa.parse(data, {
+        complete: this.updateData
+      }))
+  }
+
+  updateData = (result) => {
+    this.setState({ data: result.data })
+  }
+
+  let hmap = new Map();
+  
+    return (
+      <div>
+        {console.log(state.data)}
+      </div>
+    )
+
 }
+
+export default Genre 
+
