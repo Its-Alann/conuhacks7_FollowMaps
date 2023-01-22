f50ed4dca5772b5d5e9fc8875c8221df0df01aa7
Alan Vadlakunta <76508163+Its-Alann@users.noreply.github.com>
2023-01-21 22:42:36 -0500

    Implemented no btn fix

--- vizify/src/App.js
+++ vizify/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Artists from './components/Artists';
 import FileParser from './components/FileParser';
 import Treemap from './components/Treemap';
+import Genre from './components/Genre';
 
 function App() {
   return (
     <div>
       <Artists/>
+      {/* <Genre/> */}
     </div>
   );
 }
